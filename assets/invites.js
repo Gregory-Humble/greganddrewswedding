aa41cf4794488cc8384b7b9f9a7aeb1402360634
Gregory Humble <57158738+Gregory-Humble@users.noreply.github.com>
2025-11-09 21:52:36 +1100

    Update invites.js

--- assets/invites.js
+++ assets/invites.js
@@ -1,15 +1,40 @@
 // assets/invites.js
 // Map of invite codes to household caps and optional default names
 window.INVITES = {
-  "EXAMPLE1": {
+  "BRWNTWN": {
     household: "The Brownlee Family",
     maxGuests: 2,
     defaultNames: ["Gillian Brownlee", "Austin Brownlee"]
   },
-  "EXAMPLE2": {
+  "HMBLE1": {
     household: "The Humble Family",
     maxGuests: 4,
     defaultNames: ["Bruce Humble", "Yoon Choi", "Tashi Humble", "Saffi Humble"]
+  },
+   "1B-DI": {
+    household: "Sally Humble",
+    maxGuests: 2,
+    defaultNames: ["Sally Humble", "Guest"]
+  },
+    "YAS-Q88N": {
+    household: "Lucinda Humble Camelleri",
+    maxGuests: 2,
+    defaultNames: ["Lucinda Humble", "Guest"]
+  },
+    "G1RLYG1RL": {
+    household: "Matt Kulesza & Arielle Sweetman",
+    maxGuests: 2,
+    defaultNames: ["Matt Kulesza", "Arielle Sweetman"]
+  },
+    "GAY4PAY": {
+    household: "Benné Kulesza & Andrew Noble",
+    maxGuests: 2,
+    defaultNames: ["Benné Kulesza", "Andrew Noble"]
+  },
+    "BYE-F3L1C1A": {
+    household: "Helen Kuriata & Rick Kwasek",
+    maxGuests: 2,
+    defaultNames: ["Helen Kuriata", "Rick Kwasek"]
   }
 };
 // leave this file as pure JS – no HTML, no comments with < or >
