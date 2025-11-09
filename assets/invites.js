b61aa0d5cd93d438b8d0f548d998776920af6338
Gregory Humble <57158738+Gregory-Humble@users.noreply.github.com>
2025-11-09 20:47:01 +1100

    Update invites.js

--- assets/invites.js
+++ assets/invites.js
@@ -4,9 +4,10 @@
 window.INVITES = {
   // EXAMPLES — replace with your real data
   "R7K4": { household: "The Humble Family",  maxGuests: 4, defaultNames: ["Bruce Humble","Yoon Choi","Tashi Humble","Saffi Humble"] },
-  "J9Q2": { household: "The Brownlee Family",    maxGuests: 2, defaultNames: ["Gillian Brownlee","Austin Brownlee"] },
-  "D3EW": { household: "Sally Humble",     maxGuests: 2, defaultNames: ["Sally Humble","Guest"] },
+  "D3EW": { household: "The Brownlee Family",    maxGuests: 2, defaultNames: ["Gillian Brownlee","Austin Brownlee"] },
+  "1BDI": { household: "Sally Humble",     maxGuests: 2, defaultNames: ["Sally Humble","Guest"] },
   "M8Z1": { household: "Lucinda Humble-Camilleri",    maxGuests: 2, defaultNames: ["Lucinda Humble-Camilleri","Guest"] },
+  "69BC": { household: "Heath Farnsworth & Luke Henson",    maxGuests: 2, defaultNames: ["Heath Farnsworth","Luke Henson"] },
 
   // ADD MORE HERE ...
 };
